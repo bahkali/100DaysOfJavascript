7105da99cccf42d67ce089b1d2efa8beaa3fad94
kaly <kalimamadou@gmail.com>
2022-03-06 13:58:24 -0800

    Added the database connect and dotenv for the connection string

--- 3-RestfupApiNode/app.js
+++ 3-RestfupApiNode/app.js
@@ -1,18 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const path = require("path");
 const postRoute = require("./route/postRoute");
+
 const app = express();
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// DB connection
+const dbUri = process.env.DB_URI;
+mongoose.Promise = global.Promise;
+mongoose.connect(dbUri).then(() => {
+  console.log("Database connected...");
+});
 // routes
 // app.get("/", (req, res) => {
 //   res.send("Test app js");
 // });
-app.use(postRoute);
+app.use("/post", postRoute);
 
 const PORT = 4000 || process.env.PORT;
 app.listen(PORT, () => {
