665a614924dbf455855fda1090f97df80ec43271
kaly <kalimamadou@gmail.com>
2022-03-08 04:41:12 -0800

    Moved the DB connection to it own fine

--- 4-StoreNodeApi/app.js
+++ 4-StoreNodeApi/app.js
@@ -1,27 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const mongoose = require("mongoose");
+const connectDb = require("./db/connect");
 const bodyParser = require("body-parser");
 //routes
 const productRoute = require("./route/productRoute");
 
 const app = express();
 const dbUri = process.env.DB_URI;
-//DB Connection
-mongoose.connect(dbUri).then((res) => {
-  console.log(`Database is connected..`);
-});
 
 //Middelware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Routes
-app.use("/product", productRoute);
+app.use("/api/v1/products", productRoute);
+
+// Error handling
 
 // Listen
-const PORT = 4500 || process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+const PORT = process.env.PORT || 4500;
+
+const start = async () => {
+  try {
+    //DB Connection
+    await connectDb(dbUri);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {}
+};
+
+start();
